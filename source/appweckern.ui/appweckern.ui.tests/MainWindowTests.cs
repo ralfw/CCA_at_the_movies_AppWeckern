88e059828fe427e3292d61fec43a38b8abfcb4e7
slieser <stefan@lieser-online.de>
2013-01-22 16:18:06 +0100

    UI implementiert

---
+++ source/appweckern.ui/appweckern.ui.tests/MainWindowTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace appweckern.ui.tests
+{
+    [TestFixture]
+    public class MainWindowTests
+    {
+        [Test, Explicit, RequiresSTA]
+        public void Show() {
+            var sut = new MainWindow();
+            sut.ShowDialog();
+        } 
+    }
+}
