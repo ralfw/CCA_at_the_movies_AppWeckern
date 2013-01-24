f03d269a7dca7fd54f92cf40c20e5aed0d053e33
Ralf Westphal <info@ralfw.de>
2013-01-24 09:23:51 +0100

    Wecker berechnet Restzeit

---
+++ source/appweckern.wecker/appweckern.wecker.tests/test_Wecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace appweckern.wecker.tests
+{
+    [TestFixture]
+    public class test_Wecker
+    {
+        [Test]
+        public void Restzeit_berechnen()
+        {
+            var sut = new Wecker();
+
+            var result = new TimeSpan(0);
+            sut.Restzeit += _ => result = _;
+
+            sut.Starten(new Tuple<DateTime, TimeSpan>(new DateTime(2013,1,24,10,0,0), new TimeSpan(0)));
+            sut.Zeitzeichen(new DateTime(2013,1,24,9,58,0));
+
+            Assert.AreEqual(new TimeSpan(0,2,0), result);
+        }
+
+        [Test]
+        public void Ohne_Weckzeit_keine_Restzeit()
+        {
+            var sut = new Wecker();
+
+            var result = new TimeSpan(0);
+            sut.Restzeit += _ => result = _;
+
+            sut.Zeitzeichen(new DateTime(2013, 1, 24, 9, 58, 0));
+
+            Assert.AreEqual(new TimeSpan(0), result);
+        }
+    }
+}
---
+++ source/appweckern.wecker/appweckern.wecker/Wecker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using appweckern.contracts;
+
+namespace appweckern.wecker
+{
+    public class Wecker : IWecker
+    {
+        private DateTime? _weckzeit;
+
+        public Wecker()
+        {
+            _starten += _ => Weckzeit_bestimmen(_, weckzeit => _weckzeit = weckzeit);
+            _zeitzeichen += uhrzeit => Restzeit_berechnen(_weckzeit, uhrzeit, _ => Restzeit(_));
+        }
+
+
+        private readonly Action<Tuple<DateTime, TimeSpan>> _starten;
+        public void Starten(Tuple<DateTime, TimeSpan> weckzeit) { _starten(weckzeit); }
+
+        private readonly Action<DateTime> _zeitzeichen;
+        public void Zeitzeichen(DateTime zeitzeichen) { _zeitzeichen(zeitzeichen); }
+
+        private Action _stoppen;
+        public void Stoppen() { _stoppen(); }
+
+
+        public event Action<TimeSpan> Restzeit;
+        public event Action Abgelaufen;
+        public event Action Gestoppt;
+
+
+        private void Weckzeit_bestimmen(Tuple<DateTime, TimeSpan> weckzeit_oder_ruhezeit, Action<DateTime> on_weckzeit)
+        {
+            on_weckzeit(weckzeit_oder_ruhezeit.Item1);
+        }
+
+        private void Restzeit_berechnen(DateTime? weckzeit, DateTime uhrzeit, Action<TimeSpan> on_restzeit)
+        {
+            if (!weckzeit.HasValue) return;
+
+            on_restzeit(weckzeit.Value.Subtract(uhrzeit));
+        }
+    }
+}
