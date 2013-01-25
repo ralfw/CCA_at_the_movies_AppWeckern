b9d480ccdd24548bbdc62bceffa58e60cbef21d7
slieser <stefan@lieser-online.de>
2013-01-25 08:22:58 +0100

    Bimmel ergänzt

---
+++ source/appweckern.bimmel/appweckern.bimmel.tests/BimmelTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace appweckern.bimmel.tests
+{
+    [TestFixture]
+    public class BimmelTests
+    {
+        private Bimmel sut;
+
+        [Test, Explicit]
+        public void Die_Bimmel_kann_läuten() {
+            sut = new Bimmel();
+            sut.Läuten();
+        }
+    }
+}
---
+++ source/appweckern.bimmel/appweckern.bimmel/Bimmel.cs
@@ -0,0 +1,27 @@
+﻿using System.Media;
+using appweckern.contracts;
+
+namespace appweckern.bimmel
+{
+    public class Bimmel : IBimmel
+    {
+        private SoundPlayer soundplayer;
+        private readonly string filename;
+
+        public Bimmel() {
+            filename = "ring.wav";
+            Init();
+        }
+
+        private void Init() {
+            soundplayer = new SoundPlayer {
+                SoundLocation = filename
+            };
+            soundplayer.Load();
+        }
+
+        public void Läuten() {
+            soundplayer.PlaySync();
+        }
+    }
+}
